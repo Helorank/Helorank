076da5a75d45b84bc05ff03553f1fb68286bec46
Kyle Vermeer <kylevermeer@gmail.com>
2013-04-14 23:58:54 -0700

    Removed old request handler

--- main.py
+++ main.py
@@ -17,8 +17,4 @@
 import webapp2
 from urls import routes
 
-class MainHandler(webapp2.RequestHandler):
-  def get(self):
-    self.response.write('Hello world!')
-
 app = webapp2.WSGIApplication(routes, debug=True)
