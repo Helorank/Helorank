1c7de4787890135a88efd732063ded0f668d1218
CodySam <codysam@gmail.com>
2013-04-21 16:19:06 -0700

    Added handlers

--- handlers/index.py
+++ handlers/index.py
@@ -4,20 +4,80 @@ from util.template import jinja_environment
 from models.rankSystem import *
 
 class IndexHandler(webapp2.RequestHandler):
-  def get(self):
-    kyleAccount = Account(facebookID="KV35")
-    kyleAccount.put()
-    codyAccount = Account(facebookID="CS22")
-    codyAccount.put()
-    pool = Pool()
-    pool.put()
-    kyle = Player.createPlayer(username="Kyle Vermeer", account=kyleAccount, pool=pool)
-    cody = Player.createPlayer(username="Cody Sam", account=codyAccount, pool=pool)
+    def get(self):
+        kyleAccount = Account(facebookID="KV35")
+        kyleAccount.put()
+        codyAccount = Account(facebookID="CS22")
+        codyAccount.put()
+        pool = Pool()
+        pool.put()
+        kyle = Player.createPlayer(username="Kyle Vermeer", account=kyleAccount, pool=pool)
+        cody = Player.createPlayer(username="Cody Sam", account=codyAccount, pool=pool)
+    
+        kyleTeam = Team.createTeamWithPlayers([kyle], description="Kyle")
+        codyTeam = Team.createTeamWithPlayers([cody], description="Cody")
+    
+        firstGame = Game.createGameWithTeams([kyleTeam,codyTeam])
+        firstGame.updateRatings()
+        template = jinja_environment.get_template('index.html')
+        self.response.out.write(template.render({}))
 
-    kyleTeam = Team.createTeamWithPlayers([kyle], description="Kyle")
-    codyTeam = Team.createTeamWithPlayers([cody], description="Cody")
-
-    firstGame = Game.createGameWithTeams([kyleTeam,codyTeam])
-    firstGame.updateRatings()
-    template = jinja_environment.get_template('index.html')
-    self.response.out.write(template.render({}))
+class aboutHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('about.html')
+        self.response.out.write(template.render({}))
+         
+class helosHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('helos.html')
+        self.response.out.write(template.render({}))
+        
+class poolsHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('pools.html')
+        self.response.out.write(template.render({}))
+        
+class gamesHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('games.html')
+        self.response.out.write(template.render({}))
+        
+class playersHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('players.html')
+        self.response.out.write(template.render({}))
+        
+class submitGameHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('submitGame.html')
+        self.response.out.write(template.render({}))
+        
+class disputeGameHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('disputeGame.html')
+        self.response.out.write(template.render({}))
+        
+class createPoolHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('createPool.html')
+        self.response.out.write(template.render({}))
+        
+class joinPoolHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('joinPool.html')
+        self.response.out.write(template.render({}))
+        
+class contactHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('contact.html')
+        self.response.out.write(template.render({}))
+        
+class blogHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('blog.html')
+        self.response.out.write(template.render({}))
+        
+class termsHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('terms.html')
+        self.response.out.write(template.render({}))
--- urls.py
+++ urls.py
@@ -2,5 +2,17 @@ import webapp2
 from webapp2_extras import routes
 
 routes = [
-  webapp2.Route(r'/',handler='handlers.index.IndexHandler')
+  webapp2.Route(r'/',handler='handlers.index.IndexHandler'),
+  webapp2.Route(r'/About',handler='handlers.index.aboutHandler'),
+  webapp2.Route(r'/Helos',handler='handlers.index.helosHandler'),
+  webapp2.Route(r'/Pools',handler='handlers.index.poolsHandler'),
+  webapp2.Route(r'/Games',handler='handlers.index.gamesHandler'),
+  webapp2.Route(r'/Players',handler='handlers.index.playersHandler'),
+  webapp2.Route(r'/SubmitGame',handler='handlers.index.submitGameHandler'),
+  webapp2.Route(r'/DisputeGame',handler='handlers.index.disputeGameHandler'),
+  webapp2.Route(r'/CreatePool',handler='handlers.index.createPoolHandler'),
+  webapp2.Route(r'/JoinPool',handler='handlers.index.joinPoolHandler'),
+  webapp2.Route(r'/Contact',handler='handlers.index.contactHandler'),
+  webapp2.Route(r'/Blog',handler='handlers.index.blogHandler'),
+  webapp2.Route(r'/Terms',handler='handlers.index.termsHandler')
 ]
