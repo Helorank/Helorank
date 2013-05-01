c4ebef294303910993f98777726d517702514fb3
Kyle Vermeer <kylevermeer@gmail.com>
2013-05-01 01:59:45 -0700

    account creation, production vs testing db

--- accounts/models.py
+++ accounts/models.py
@@ -4,12 +4,13 @@ from django.db import models
 class Account(models.Model):
   email = models.CharField(max_length=200)
   password = models.CharField(max_length=200)
-  creation_date = models.DateTimeField('date created')
-  facebook_ID = models.CharField(max_length=200)
-  twitter_ID = models.CharField(max_length=200)
-  age = models.IntegerField(default=0)
-  first_name = models.CharField(max_length=200)
-  last_name = models.CharField(max_length=200)
+  creation_date = models.DateTimeField('date created', auto_now_add=True)
+  facebook_ID = models.CharField(max_length=200, blank=True)
+  twitter_ID = models.CharField(max_length=200, blank=True)
+  age = models.IntegerField(null=True)
+  username = models.CharField(max_length=200)
+  first_name = models.CharField(max_length=200, blank=True)
+  last_name = models.CharField(max_length=200, blank=True)
 
   def __unicode__(self):
     return self.email
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.contrib.auth.hashers import *
+from accounts.models import Account
 
 # Create your views here.
+def create_account_handler(request):
+  if request.method == 'GET':
+    return create_account_handler_get(request)
+  elif request.method == 'POST':
+    return create_account_handler_post(request)
+  
+
+def create_account_handler_get(request):
+  return render(request,'accounts/createAccount.html',{ "error" : "No error"})
+
+def create_account_handler_post(request):
+  post_dict = request.POST
+  email = post_dict["email"]
+  #Check for already used password
+  if Account.objects.filter(email=email):
+    # e-mail has already been used
+    return render(request, 'accounts/createAccount.html',{ "error" : "E-mail has already been used" })
+  else:
+    password = post_dict["password"]
+    username = post_dict["username"]
+    encrypted_password = make_password(password)
+    print encrypted_password
+    newAccount = Account(email = email, username=username, password=encrypted_password)
+    newAccount.save()
+    return render(request,'accounts/accountCreated.html',{ "account" : newAccount })
